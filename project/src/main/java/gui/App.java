600df732e10b2d58995978b6a4ad6e2ed02309ac
superiorshrimp <szymon.zychowicz300701@gmail.com>
2023-01-14 12:55:30 +0100

    update 1

--- project/src/main/java/gui/App.java
+++ project/src/main/java/gui/App.java
@@ -24,61 +24,28 @@ public class App extends javafx.application.Application {
     @Override
     public void start(Stage primaryStage) throws Exception{
         ArrayList<String> labels = Phone.getLabels();
+
         ExpertModule expertModule = new ExpertModule(labels, this);
         UserModule userModule = new UserModule(labels);
+        MultiExpertModule expertsModule = new MultiExpertModule(labels, this);
+        ManageExperts manageExpertsModule = new ManageExperts(labels, this);
 
         FlowPane flowPane = new FlowPane(Orientation.VERTICAL);
         flowPane.setHgap(5);
         flowPane.setAlignment(Pos.CENTER);
         flowPane.setPadding(new Insets(0, 0, 0, 0)); // set top, right, bottom, left
 
-        HBox hBox = new HBox();
-        Button expertButton = new Button("I`m an expert");
-        expertButton.setFont(Font.font("Verdana", 20));
-        Button userButton = new Button("I`m a user");
-        userButton.setFont(Font.font("Verdana", 20));
-
-        hBox.setAlignment(Pos.CENTER);
-        hBox.getChildren().addAll(expertButton, userButton);
-
         Button gitUrl = new Button("Copy Github repo URL");
         gitUrl.setFont(Font.font("Verdana", 20));
-
         gitUrl.setOnAction(event -> {
             final Clipboard clipboard = Clipboard.getSystemClipboard();
             final ClipboardContent content = new ClipboardContent();
-            content.putString("https://github.com/superiorshrimp/miapd");
+            content.putString("https://github.com/superiorshrimp/miapd_enhanced");
             clipboard.setContent(content);
         });
 
-        Text title = new Text("AHP phone preference ranking app");
-        title.setFont(Font.font("Verdana", 20));
-
-        VBox vBox = new VBox();
-        vBox.setSpacing(25);
-        vBox.setAlignment(Pos.CENTER);
-        vBox.getChildren().addAll(title, gitUrl, hBox);
-
-        BorderPane borderPane = new BorderPane();
-        borderPane.setCenter(flowPane);
-        borderPane.setCenter(vBox);
-        BorderPane.setAlignment(vBox, Pos.CENTER);
-        borderPane.setMinHeight(100);
-        borderPane.setMaxHeight(200);
-        borderPane.setMinWidth(500);
-
-        Scene scene = new Scene(borderPane);
-
-        primaryStage.setTitle("App");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-        primaryStage.setOnCloseRequest((WindowEvent we) -> System.exit(0));
-
-        expertButton.setOnAction(event -> {
-            primaryStage.close();
-            expertModule.start();
-        });
-
+        Button userButton = new Button("User mode");
+        userButton.setFont(Font.font("Verdana", 20));
         userButton.setOnAction(event -> {
             primaryStage.close();
             try {
@@ -87,5 +54,54 @@ public class App extends javafx.application.Application {
                 throw new RuntimeException(e);
             }
         });
+
+        Button expertButton = new Button("Single expert mode");
+        expertButton.setFont(Font.font("Verdana", 20));
+        expertButton.setOnAction(event -> {
+            primaryStage.close();
+            expertModule.start();
+        });
+
+        Button expertsButton = new Button("Multi-expert mode");
+        expertsButton.setFont(Font.font("Verdana", 20));
+        expertsButton.setOnAction(event -> {
+            primaryStage.close();
+            expertsModule.start();
+        });
+
+        Button manageExpertsButton = new Button("Manage experts");
+        manageExpertsButton.setFont(Font.font("Verdana", 20));
+        manageExpertsButton.setOnAction(event -> {
+            primaryStage.close();
+            manageExpertsModule.start();
+        });
+
+        Text title = new Text("Enhanced AHP phone preference ranking app");
+        title.setFont(Font.font("Verdana", 20));
+
+        VBox modesBox = new VBox();
+        modesBox.setSpacing(5);
+        modesBox.setAlignment(Pos.CENTER);
+        modesBox.getChildren().addAll(userButton, expertButton, expertsButton);
+
+        VBox vBox = new VBox();
+        vBox.setSpacing(25);
+        vBox.setAlignment(Pos.CENTER);
+        vBox.getChildren().addAll(title, gitUrl, modesBox, manageExpertsButton);
+
+        BorderPane borderPane = new BorderPane();
+        borderPane.setCenter(flowPane);
+        borderPane.setCenter(vBox);
+        BorderPane.setAlignment(vBox, Pos.CENTER);
+        borderPane.setMinHeight(200);
+        borderPane.setMaxHeight(500);
+        borderPane.setMinWidth(500);
+
+        Scene scene = new Scene(borderPane);
+
+        primaryStage.setTitle("App");
+        primaryStage.setScene(scene);
+        primaryStage.show();
+        primaryStage.setOnCloseRequest((WindowEvent we) -> System.exit(0));
     }
 }
